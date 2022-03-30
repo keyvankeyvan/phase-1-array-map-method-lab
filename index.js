2eb6453f9210af244ebf99fc74d3d0847d8970a2
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-30 11:47:47 -0400

    commit tree ac93c59e3c3db718986b36a4fb45ae6828d160de

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const moddedTutorials = tutorials.map(phrase => {
+    // RETURNS AN ARRAY OF SPLIT WORDS
+    const splitPhrase = phrase.split(' ')
+
+    // UPPERCASES FIRST LETTER OF EACH WORD
+    const upperCasedArr = splitPhrase.map(word => {
+      return word[0].toUpperCase() + word.substring(1)
+    })
+
+    // WE JOIN THE WORDS W/ A SPACE IN BETWEEN
+    return upperCasedArr.join(' ')
+  })
+
+  // RETURN THE NICE ARRAY TO THE MAIN .MAP
+  //console.log(moddedTutorials)
+  return moddedTutorials
 }
+
+//console.log(titleCased())
